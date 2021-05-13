48cdedfab08ae34ae97c4fd556fc1724a7efb11d
Volodymyr Shturma <shturma.v@gmail.com>
2021-05-13 14:43:16 +0300

    Fix typos

--- vcloud/provider.tf
+++ vcloud/provider.tf
@@ -3,7 +3,7 @@ provider "vcd" {
   password             = var.vcd_pass
   auth_type            = "integrated"
   org                  = var.vcd_org
-  vdc                  = var.vdc_vdc
-  url                  = var.vdc_url
+  vdc                  = var.vcd_vdc
+  url                  = var.vcd_url
   allow_unverified_ssl = var.allow_unverified_ssl
 }
