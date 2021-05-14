1c523810c444c0067be40904c3e3dfca45c893f2
Volodymyr Shturma <shturma.v@gmail.com>
2021-05-14 19:38:27 +0300

    Add cp vm

--- vcloud/main.tf
+++ vcloud/main.tf
@@ -6,3 +6,23 @@ resource "vcd_vapp_org_network" "onapp-public" {
   vapp_name = vcd_vapp.onapp.name
   org_network_name = "10.77.0.1/24"
 }
+
+resource "vcd_vapp_vm" "cp" {
+  vapp_name = vcd_vapp.onapp.name
+  name = "cp"
+  computer_name = "cp"
+  catalog_name = "ShturmaCatalog"
+  template_name = "CentOS7template"
+  memory = 4096
+  cpus = 2
+  network {
+    type = "org"
+    name = "management"
+    ip_allocation_mode = "MANUAL"
+    ip = "10.77.0.235"
+    connected = true
+  }
+  customization {
+    enabled = true
+  }
+}
