14c13a05db3a46c717c5ec7378c480c484200940
Volodymyr Shturma <shturma.v@gmail.com>
2021-05-14 19:05:43 +0300

    Remove direct network block

--- vcloud/main.tf
+++ vcloud/main.tf
@@ -1,13 +1,8 @@
-resource "vcd_network_direct" "onapp-public" {
-  name = "onapp-public"
-  external_network = "app"
-}
-
 resource "vcd_vapp" "onapp" {
   name = "onapp"
 }
 
 resource "vcd_vapp_org_network" "onapp-public" {
   vapp_name = vcd_vapp.onapp.name
-  org_network_name = vcd_network_direct.onapp-public.name
+  org_network_name = "10.77.0.1/24"
 }
