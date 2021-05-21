b648f0f52b83657834dc3468255fffe4a16e50b9
Volodymyr Shturma <shturma.v@gmail.com>
2021-05-21 15:49:12 +0300

    Add OnApp provider config

---
+++ onapp/provider.tf
@@ -0,0 +1,6 @@
+provider "onapp" {
+  allow_unverified_ssl  = true
+  email                 = var.onapp_email
+  token                 = var.onapp_token
+  url                   = var.onapp_url
+}
---
+++ onapp/variables.tf
@@ -0,0 +1,11 @@
+variable "onapp_email" {
+  description = "OnApp user login"
+}
+
+variable "onapp_token" {
+  description = "OnApp user password"
+}
+
+variable "onapp_url" {
+  description = "OnApp Control Panel URL"
+}
