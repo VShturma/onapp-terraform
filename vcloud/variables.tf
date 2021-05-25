61ee506b8886d36c8c92085b7e34eaa66de634a8
Volodymyr Shturma <shturma.v@gmail.com>
2021-05-25 17:07:18 +0300

    TMP 2

--- vcloud/variables.tf
+++ vcloud/variables.tf
@@ -30,6 +30,10 @@ variable "allow_unverified_ssl" {
 # General vCloud vApp config #
 ##############################
 
+variable "vcd_vapp_name" {}
+
+variable "vcd_vapp_network_name" {}
+
 variable "vcd_vapp_network_name" {
   description = "vApp org network name"
   default = "10.77.0.1/24"
@@ -44,3 +48,13 @@ variable "vcd_template_name" {
   description = "Template name for VMs"
   default = "CentOS7tmpl"
 }
+
+#######################
+# vCloud CP VM config #
+#######################
+
+variable "vcd_cp_name" {}
+
+variable "vcd_cp_memory" {}
+
+variable "vcd_cp_cpus" {}
