eac20583d9ba332161ff23b95f20425f5b2701f2
Volodymyr Shturma <shturma.v@gmail.com>
2021-05-26 16:17:56 +0300

    TMP 7

--- vcloud/main.tf
+++ vcloud/main.tf
@@ -1,12 +1,12 @@
 module "vcloud" {
   source = "./modules/vcloud"
 
-  vapp_name = var.vcd_vapp_name
+  vcd_vapp_name = var.vcd_vapp_name
   org_network_name = var.vcd_org_network_name
-  vm_cp_name = var.vcd_vm_cp_name
-  vm_cp_computer_name = var.vcd_cp_name
-  vm_cp_catalog_name = var.vcd_catalog_name
-  vm_cp_template_name = var.vcd_template_name
-  vm_cp_memory = var.vcd_vm_cp_memory
-  vm_cp_cpus = var.vcd_vm_cp_cpus
+  vcd_vm_cp_name = var.vcd_vm_cp_name
+  vcd_cp_name = var.vcd_cp_name
+  vcd_catalog_name = var.vcd_catalog_name
+  vcd_template_name = var.vcd_template_name
+  vcd_vm_cp_memory = var.vcd_vm_cp_memory
+  vcd_vm_cp_cpus = var.vcd_vm_cp_cpus
 }
